f999594cf8e0d3ec5f13013d336e098ca3f60893
Matheus Vilas Boas Borges <matheusvbborges@hotmail.com>
2024-09-25 10:53:52 -0300

    Organização dos pacotes

---
+++ code/ManutencaoVeicular/src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sejam bem vindos ao serviço de Manutenção Veicular!!!");
+    }
+}
